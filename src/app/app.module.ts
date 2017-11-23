1f46aa4b91717fa0825702c5d0769c17e40e31e0
rafszul <rafszul@gmail.com>
2017-11-23 09:50:40 +0000

    components user-login user-profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// modules
+
 import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
+import { MaterialModule} from './core/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// components
 
 import { AppComponent } from './app.component';
+import { UserLoginComponent } from './users/user-login/user-login.component';
+import { UserProfileComponent } from './users/user-profile/user-profile.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLoginComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
