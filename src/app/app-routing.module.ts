3756845d926d806f0121c1632cb99ee4c4ba400a
rafal szulczewski <rafal.szulczewski@smartxpo.com>
2017-12-18 14:02:36 +0000

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LostComponent } from './lost/lost.component';
 import { UserLoginComponent } from './users/user-login/user-login.component';
 import { UserProfileComponent } from './users/user-profile/user-profile.component';
+import { HomeComponent } from './home/home.component';
+import { CallbackComponent } from './callback/callback.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'callback', component: CallbackComponent },
   { path: 'login', component: UserLoginComponent },
   { path: 'profile', component: UserProfileComponent },
   { path: '**', component: LostComponent }
