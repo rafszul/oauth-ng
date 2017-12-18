e8d4ce2a91232a80c2ff22b2d030710e534c3654
rafal szulczewski <rafal.szulczewski@smartxpo.com>
2017-12-18 20:54:00 +0000

    seems working - need sleep

--- src/app/callback/callback.component.ts
+++ src/app/callback/callback.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthHttp } from 'angular2-jwt';
+import 'rxjs/add/operator/map';
+import { Title } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'ng-fire-callback',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CallbackComponent implements OnInit {
 
-  constructor() { }
+  // API_URL: string = 'http://<your-application-domain>/api';
+  pageTitle = 'Home / Events';
+  API_URL: string = 'https://slozgrpoei.execute-api.eu-west-1.amazonaws.com/int_v3_smartxpo_com/event/';
+  event: string;
+
+  constructor(public authHttp: AuthHttp, public title: Title) {}
+
+  public securedcallback(): void {
+    this.event = '';
+    this.authHttp.get(`${this.API_URL}/event`)
+      .map(res => res.json())
+      .subscribe(
+        data => this.event = data.event,
+        error => this.event = error,
+        () => console.log('Request Complete')
+      );
+  }
 
   ngOnInit() {
+    this.title.setTitle(this.pageTitle);
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'ng-fire-home',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  pageTitle = 'Home';
+
+  constructor(public title: Title) { }
 
   ngOnInit() {
+    this.title.setTitle(this.pageTitle);
   }
 
 }
