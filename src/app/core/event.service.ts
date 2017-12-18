c9e47388642a8e6d60182a2834221def6af162ad
rafal szulczewski <rafal.szulczewski@smartxpo.com>
2017-12-18 15:23:56 +0000

    event service

---
+++ src/app/core/event.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+
+@Injectable()
+export class EventService {
+
+  constructor() { }
+
+}
